3875ecf7a6d4b22dcee12e8982abc642701d6bf6
Alberto <alberto0o095bm@gmail.com>
2016-11-09 18:24:48 +0100

    Servicio de Receta y Quantity terminados :8ball:
    Sujetos a cambios aún pero ta to perfe

---
+++ src/main/java/services/QuantityService.java
@@ -0,0 +1,65 @@
+package services;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import domain.Quantity;
+import repositories.QuantityRepository;
+
+
+
+@Service
+@Transactional
+public class QuantityService {
+	
+	//managed repository-------------------
+	@Autowired
+	private QuantityRepository quantityRepository;
+	
+	//supporting services-------------------
+	
+	//Basic CRUD methods-------------------
+	
+	public Quantity create(){
+		
+		Quantity created;
+		created = new Quantity();
+		return created;
+	}
+	
+	public Quantity findOne(int quantityId){
+		
+		Quantity retrieved;
+		retrieved = quantityRepository.findOne(quantityId);
+		return retrieved;
+	}
+
+	public Quantity save(Quantity quantity){
+		
+		Quantity saved = quantityRepository.save(quantity);
+		
+		return saved;
+		
+	}
+	
+	public void delete(Quantity quantity){
+		
+		quantityRepository.delete(quantity);
+		
+	}
+	
+	//Our other Bussiness methods
+	
+	public Quantity createCopy(Quantity quantity){
+		
+		Quantity copied = this.create();
+		copied.setIngredient(quantity.getIngredient());
+		copied.setQuantity(quantity.getQuantity());
+		copied.setUnit(quantity.getUnit());
+		
+		return copied;
+	}
+	
+}
