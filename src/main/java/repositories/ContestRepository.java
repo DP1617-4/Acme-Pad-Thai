c4e269a3c1e216ead4951536f6f996239255a8ab
Alberto <alberto0o095bm@gmail.com>
2016-11-11 16:36:23 +0100

    Commiting The changes in the queries
    Array instead of collection of Double and another Query. And added a
    score for a copy of a recipe for testing purposes

--- src/main/java/repositories/ContestRepository.java
+++ src/main/java/repositories/ContestRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Contest;
+import domain.Recipe;
 
 @Repository
 public interface ContestRepository extends JpaRepository<Contest, Integer>{
@@ -16,5 +17,10 @@ public interface ContestRepository extends JpaRepository<Contest, Integer>{
 	
 	@Query("select c from Contest c where c.qualified.size = (select max(c.qualified.size) from Contest c)")
 	Contest getContestWithMoreRecipesQualified();
-
+	
+	//This method returns the recipes qualified for a certain contest that have a score ordered in descending order
+	//This needs an auxiliary method in the service, maybe another query
+	@Query("select c.qualified from Contest c join left join c.qualified q left join q.scores s where c.id = ?1 order by sum(case when s.likes = 1 then 1 else 0 end) DESC ")
+	Collection<Recipe> findBestRecipes(int contestId); 
+	
 }
